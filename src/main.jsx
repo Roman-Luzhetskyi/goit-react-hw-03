d5a6d1181d0e1da2e1558526e15ad9a9619f1137
Roman-Luzhetskyi <113299200+Roman-Luzhetskyi@users.noreply.github.com>
2024-08-09 14:24:55 +0200

    comm-3

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+
+function App() {
+  return <div>
+    <h1>pega title</h1>
+  </div>
+}
+
+
+
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
